a4a066d4f8acc61b269aed98b2f9ee7b274deab5
Juhyung Yoon <ep160101@gmail.com>
2018-03-31 17:29:39 -0400

    Add svg icons and formatted

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,32 @@
 import React from 'react';
+import img from '../img/logo.png';
+import Icon from './icon';
+import UserPic from '../img/user.jpg';
 
-const Header = () => <header className="header">Header!</header>;
+const Header = () => (
+  <header className="header">
+    <img src={img} alt="trillo logo" className="logo" />
+    <form action="#" className="search">
+      <input type="text" className="search__input" placeholder="Search hotels" />
+      <button className="search__button">
+        <Icon className="search__icon" icon="icon-magnifying-glass" />
+      </button>
+    </form>
+    <nav className="user-nav">
+      <div className="user-nav__icon-box">
+        <Icon className="user-nav__icon" icon="icon-bookmark" />
+        <span className="user-nav__notification">7</span>
+      </div>
+      <div className="user-nav__icon-box">
+        <Icon className="user-nav__icon" icon="icon-chat" />
+        <span className="user-nav__notificatioon">13</span>
+      </div>
+      <div className="user-nav__user">
+        <img src={UserPic} alt="User photo" className="user-nav__user-photo" />
+        <span className="user-nav__user-name">Edd</span>
+      </div>
+    </nav>
+  </header>
+);
 
 export default Header;
--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
 import './css/style.css';
+import './img/sprite.svg';
 
 render(<App />, document.querySelector('#root'));

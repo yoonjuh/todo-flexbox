bc8a95e053d8753525236322a8da0ec79ea1fd5d
Juhyung Yoon <ep160101@gmail.com>
2018-04-04 22:36:43 -0400

    Add Hotel overview

---
+++ src/components/HotelHeader.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Icon from './icon';
+
+const HotelHeader = props => (
+  <div className="overview">
+    <h1>Hotel Las Palmas</h1>
+  </div>
+);
+
+export default HotelHeader;
